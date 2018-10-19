d4a8b64a2d88344cd31e70ad19733562de422a3f
mk203 <mk203@duke.edu>
2018-10-19 10:04:20 -0400

    Working on collision logic

--- js/app.js
+++ js/app.js
@@ -1,37 +1,14 @@
-//Constructor Function is a blueprint for Objects
-//We can use constructors to ensure each object has the same properties and includes the same methods
-//Think of a constructor like a little factory that can create an endless number of similar objects
-//We need to create a constructor for player objects. A user will be able to select their player.
-//Using constructors is a two-step process: define a constructor and then use it to create Objects
-//We want a constructor that we can use to create players, and more specifically players with different names, images, getContext
-//Player class
-  //constructor
-    //properties
-      //The position a player is in via x and y coordinates
-      // x position
-      // y position
-      // image
-    //methods
-      //update position -- this method get run every cycle of the game engine loop. It's checking
-          //our Player's position on the board in relation to whatever we want.
-            //Did enemey collide with player? check for collision function. -- are x and y coordinates equal to enemies?
-            //Did it reach the top row of the grid -- check for victory that will check if the hero's x and y coordinates match that of the final tiles.
-          //playerDraw -- draw or redraw our player to the board every loop through the main game loop.
-              //Needs an image
-              //Needs an x/y coordinate position
-          //Keyboard handle input -- event lister to fire this method when key is pressed and make changes to x and y
-          //Reset player
-              // set x and y to starting x and y coordinates
-
 //class is a newer syntax ES6+ while the constructor version has been around for a while.
 //Below is a class declaration
 
 let sprite = 'images/char-girl.png'
 class Player {
-  constructor(sprite ) {
+  constructor(sprite) {
     //101 is the width of each block and 83 is the height. Found from engine.js
     this.xValue = 101;
     this.yValue = 83;
+    this.width = 101;
+    this.height = 171;
     //We want to start at a distance of 101 *2 horizontally
     this.xStart = this.xValue * 2;
     //We want to start at a distance of 101 *5 vertically
@@ -75,43 +52,29 @@ class Player {
 
   }
 
-update() {
-  for (let enemy of allEnemies) {
-console.log (this.y + "-" + enemy.y)
+  update() {
+    for (let enemy of allEnemies) {
+      console.log(this.y + "-" + enemy.y)
 
-let enemyWidth = 101;
-let enemyHeight = 171;
-
-if ((this.x < enemy.x + enemy.enemyWidth) &&
-   (this.x + this.xValue > enemy.x) &&
-   (this.y < enemy.y + enemy.enemyHeight) &&
-   (this.y + this.yValue > enemy.y)) {
-  alert("collison!");
-}
+      let enemyWidth = 101;
+      let enemyHeight = 171;
 
 
+      if (this.x < enemy.x + enemy.enemyWidth && this.x + this.width > enemy.x && this.y < enemy.y + enemy.enemyHeight && this.y + this.height > enemy.y) {
+        alert("collison!");
+      }
+
+
+    }
 
 
   }
 
-
-
-
 }
-
-}
-
 
 // Enemies our player must avoid
 class Enemy {
 
-  // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    //x and y position of enemy
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
   constructor(x, y, speed) {
     this.x = x;
     //so it starts a little above the edge of the row
@@ -122,23 +85,20 @@ class Enemy {
     this.yValue = 83;
   }
 
-  // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-// You should multiply any movement by the dt parameter
-// which will ensure the game runs at the same speed for
-// all computers.
+
+  // Parameter: dt, a time delta between ticks
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for all computers.
   update(dt) {
     if (this.x < 505) {
       this.x = this.x + this.speed * dt;
-    }
-
-    else {
+    } else {
       this.x = 0;
     }
 
   }
 
-// Draw the enemy on the screen, required method for game
+  // Draw the enemy on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
@@ -146,11 +106,7 @@ class Enemy {
 
 };
 
-
-
-
-
-
+//create our player and enemy objects
 const player = new Player(sprite);
 const enemy1 = new Enemy(0, 83, 100);
 const enemy2 = new Enemy(83, 101, 200);
